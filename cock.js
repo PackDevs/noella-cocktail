0d8655c8c4d171848506dcbffdc6503a0336dfb6
UWAYONoella <noellawayo2019@gmail.com>
2022-08-25 13:19:10 +0200

    Feat:the cocktail

--- cock.js
+++ cock.js
@@ -52,7 +52,10 @@ document.addEventListener("DOMContentLoaded",e=>{
 let searching=(value)=>{
     drinks.innerHTML=" ";
     const search=images.filter(item=>item.name.toLowerCase().includes(value.toLowerCase()))
-    search.map(item=>displayImage(item.src,item.name))
+    if(searching=includes(value)){
+        search.map(item=>{displayImage(item.src,item.name)})
+    }
+    search.map(item=>{displayImage(item.src,item.name)})
 }
 button1.addEventListener("keyup",e=>{
 searching(button1.value);
